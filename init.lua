4dd1ddb035fb0b21718d04c8cc9865ec827f0e40
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2021-07-10 09:32:27 -0300

    init.lua: opções modificadas para arquivos .txt e .sh

--- init.lua
+++ init.lua
@@ -3,17 +3,27 @@
 local plugin = 'plugins/ins-completion/'
 
 local d = require(plugin..'complete-dict')
-local k = require(plugin..'complete-keyword')
+local K = require(plugin..'complete-keyword')
 -- dictfiles =  { 
 -- 	path = '~/.local/share/dict/',
 -- 	text = '/usr/share/words',
 -- }
 
-local long_words = "wl.sh -W"
-table.insert(k.completeopts.default, long_words)
--- k.completeopts.markdown = { "cat " .. dictfiles.path .. "text" }
+local long_words = "wordlist -W"
+table.insert(K.completeopts.default, long_words)
+K.completeopts.text = {
+	long_words,
+	"tr -s '[:blank:][:punct:]' '\n'",
+	"cat " .. dictfiles.path .. "text", -- can't precede any command that uses stdin
+}
+K.completeopts.bash = {
+	long_words,
+	"tr -cs '[:alnum:]_' '\n'",
+	"dmenu_path",
+	"cat " .. dictfiles.path .. "bash",
+}
 
-local l = require(plugin..'complete-line')
--- l.vis_menu_lines = 4 -- default
+local L = require(plugin..'complete-line')
+-- L.vis_menu_lines = 4 -- default
 
-local c = require(plugin..'complete-char')
+local C = require(plugin..'complete-char')
