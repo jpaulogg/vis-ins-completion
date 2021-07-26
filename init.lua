c522431e11763dbb3fdb10ceb782de001b795782
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2021-07-25 21:55:13 -0300

    init.lua: better examples.

--- init.lua
+++ init.lua
@@ -2,21 +2,21 @@
 
 local plugin = 'plugins/ins-completion/'
 
-local d = require(plugin..'complete-dict')
-local k = require(plugin..'complete-keyword')
--- dictfiles =  { 
--- 	path = '~/.local/share/dict/',
+local D = require(plugin..'complete-dict')
+local K = require(plugin..'complete-keyword')
+local L = require(plugin..'complete-line')
+local C = require(plugin..'complete-char')
+
+-- DICTFILES =  { 
+-- 	dirname = '~/.local/share/dict/',
 -- 	text = '/usr/share/words',
 -- }
 
--- local long_words = "tr '[:alnum:]_.:-' '\n'"
--- table.insert(k.completeopts.default, long_words)
--- k.completeopts.bash = {
-	-- "tr -cs '[:alnum:]_' '\n'",  -- uses file content, so it should be the first
+-- L.vis_menu_lines = 4 -- default
+
+K.completeopts.default.w = [[tr '[:alnum:]_.:-' '\n']]  -- WORDS instead of words
+-- K.completeopts.bash = {
+	-- "tr -cs '[:alnum:]_' '\n'",  -- only one command will read file content
 	-- "dmenu_path",                -- dmenu_path script ships with dmenu(1)
 -- }
 
-local l = require(plugin..'complete-line')
--- l.vis_menu_lines = 4 -- default
-
-local c = require(plugin..'complete-char')
