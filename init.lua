4ca75f8b511a174aeee71485b5a36896ef06497b
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2021-07-25 21:36:32 -0300

    melhorando comandos da tabela "bash" e nomes de variáveis

--- init.lua
+++ init.lua
@@ -2,26 +2,27 @@
 
 local plugin = 'plugins/ins-completion/'
 
-local d = require(plugin..'complete-dict')
+local D = require(plugin..'complete-dict')
 local K = require(plugin..'complete-keyword')
--- dictfiles =  { 
--- 	path = '~/.local/share/dict/',
+local L = require(plugin..'complete-line')
+local C = require(plugin..'complete-char')
+
+-- DICTFILES =  { 
+-- 	dirname = '~/.local/share/dict/',
 -- 	text = '/usr/share/words',
 -- }
 
-table.insert(K.completeopts.default, long_words)
-K.completeopts.text = {
--- apenas o primeiro comando lê a entrada padrão (no caso, o conteúdo do arquivo)
-	"tr -s '[:blank:][:punct:]' '\n'",
-	"cat " .. dictfiles.path .. "text",
-}
-K.completeopts.bash = {
-	"wl.sh -W",
-	"dmenu_path",
-	"cat " .. dictfiles.path .. "bash",
-}
-
-local L = require(plugin..'complete-line')
 -- L.vis_menu_lines = 4 -- default
 
-local C = require(plugin..'complete-char')
+K.completeopts.default.w =  "wl.sh -W"
+K.completeopts.bash = {
+	string.format("cat - $HISTFILE '%s' | wl.sh -W",
+		DICTFILES.dirname .. "bash"),
+	"dmenu_path",
+}
+K.completeopts.text = {
+	"tr -s '[:blank:][:punct:]' '\n'",
+	"cat " .. DICTFILES.dirname .. "text",
+}
+
+
